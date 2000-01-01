a3bed24f0731f6b69efee5220bb2bf1b4de46c65
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Green repair and reproduction for 67fbf723-d81d-41f2-a55e-958e8049956a-bug-1

--- internal/workflow/workflow.go
+++ internal/workflow/workflow.go
@@ -101,10 +101,20 @@ func (s *Service) Search(f ListFilter) (ListResult, error) {
 	return ListResult{Items: items, Total: len(items)}, nil
 }
 
-func (s *Service) mutate(id, key string, expected *int64, fn func(*domain.Trial) error) (*domain.Trial, error) {
+// idempotencyActionKey 将动作与幂等键组合，使同一试验的不同动作即使复用相同的
+// Idempotency-Key 也能各自独立缓存与重放。空键保持不启用幂等缓存的原有行为。
+func idempotencyActionKey(action, key string) string {
+	if key == "" {
+		return ""
+	}
+	return action + "\x00" + key
+}
+
+func (s *Service) mutate(id, action, key string, expected *int64, fn func(*domain.Trial) error) (*domain.Trial, error) {
 	unlock := s.lock(id)
 	defer unlock()
-	if out, ok := s.Store.LookupIdempotent(id, key); ok {
+	cacheKey := idempotencyActionKey(action, key)
+	if out, ok := s.Store.LookupIdempotent(id, cacheKey); ok {
 		return out, nil
 	}
 	t, err := s.Store.Get(id)
@@ -124,7 +134,7 @@ func (s *Service) mutate(id, key string, expected *int64, fn func(*domain.Trial)
 	if err = fn(t); err != nil {
 		return nil, err
 	}
-	return s.Store.Save(t, old, key)
+	return s.Store.Save(t, old, cacheKey)
 }
 
 func (s *Service) PreviewProtocol(id string, p domain.TreatmentProtocol) (domain.ProtocolPreview, error) {
@@ -142,53 +152,53 @@ func (s *Service) PreviewProtocol(id string, p domain.TreatmentProtocol) (domain
 }
 
 func (s *Service) LockProtocol(id, key string, p domain.TreatmentProtocol) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.LockProtocol(p) })
+	return s.mutate(id, "protocol", key, nil, func(t *domain.Trial) error { return t.LockProtocol(p) })
 }
 func (s *Service) LockProtocolChecked(id, key string, expected int64, digest, actor string, p domain.TreatmentProtocol) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error { return t.LockProtocolPreview(p, digest, expected, actor) })
+	return s.mutate(id, "protocol", key, &expected, func(t *domain.Trial) error { return t.LockProtocolPreview(p, digest, expected, actor) })
 }
 func (s *Service) Start(id, key string) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.StartObserving() })
+	return s.mutate(id, "start", key, nil, func(t *domain.Trial) error { return t.StartObserving() })
 }
 func (s *Service) StartChecked(id, key string, expected int64, actor string) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error { return t.StartObservingBy(actor) })
+	return s.mutate(id, "start", key, &expected, func(t *domain.Trial) error { return t.StartObservingBy(actor) })
 }
 func (s *Service) Observe(id, key string, o domain.DailyObservation) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.AddObservation(o) })
+	return s.mutate(id, "observe", key, nil, func(t *domain.Trial) error { return t.AddObservation(o) })
 }
 func (s *Service) ObserveBatch(id, key string, expected int64, day int, observations []domain.DailyObservation, actor string) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error { return t.AddObservationBatch(day, observations, actor) })
+	return s.mutate(id, "observe", key, &expected, func(t *domain.Trial) error { return t.AddObservationBatch(day, observations, actor) })
 }
 func (s *Service) Deviation(id, key string, d domain.Deviation) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.AddDeviation(d) })
+	return s.mutate(id, "deviations", key, nil, func(t *domain.Trial) error { return t.AddDeviation(d) })
 }
 func (s *Service) DeviationChecked(id, key string, expected int64, d domain.Deviation, actor string) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error { return t.AddDeviationBy(d, actor) })
+	return s.mutate(id, "deviations", key, &expected, func(t *domain.Trial) error { return t.AddDeviationBy(d, actor) })
 }
 func (s *Service) Resolve(id, key, devID string) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.ResolveDeviation(devID) })
+	return s.mutate(id, "resolve", key, nil, func(t *domain.Trial) error { return t.ResolveDeviation(devID) })
 }
 func (s *Service) ResolveChecked(id, key string, expected int64, devID, responsible, completion string, observationIDs []string, actor string) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error {
+	return s.mutate(id, "resolve", key, &expected, func(t *domain.Trial) error {
 		return t.ResolveDeviationWithEvidence(devID, responsible, completion, observationIDs, actor)
 	})
 }
 func (s *Service) Submit(id, key string) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.ReadyForReview() })
+	return s.mutate(id, "submit", key, nil, func(t *domain.Trial) error { return t.ReadyForReview() })
 }
 func (s *Service) SubmitChecked(id, key string, expected int64, actor string) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error { return t.SubmitForReview(actor) })
+	return s.mutate(id, "submit", key, &expected, func(t *domain.Trial) error { return t.SubmitForReview(actor) })
 }
 func (s *Service) Review(id, key, decision, reason, reviewer string) (*domain.Trial, error) {
-	return s.mutate(id, key, nil, func(t *domain.Trial) error { return t.Review(decision, reason, reviewer) })
+	return s.mutate(id, "review", key, nil, func(t *domain.Trial) error { return t.Review(decision, reason, reviewer) })
 }
 func (s *Service) ReviewChecked(id, key string, expected int64, decision, conclusion, reviewer, digest string, issues []domain.ReviewIssueInput) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error {
+	return s.mutate(id, "review", key, &expected, func(t *domain.Trial) error {
 		return t.ReviewWithChecklist(decision, conclusion, reviewer, digest, issues)
 	})
 }
 func (s *Service) Correct(id, key string, expected int64, issueID, note string, refs []string, confirm bool, actor string) (*domain.Trial, error) {
-	return s.mutate(id, key, &expected, func(t *domain.Trial) error { return t.SubmitCorrection(issueID, note, refs, confirm, actor) })
+	return s.mutate(id, "corrections", key, &expected, func(t *domain.Trial) error { return t.SubmitCorrection(issueID, note, refs, confirm, actor) })
 }
 
 func (s *Service) Details(id string, filters ...EventFilter) (map[string]any, error) {
